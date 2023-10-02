114e13fdb0d43998b8c87ed4303b8ff9fadde36f
akfathalla <abdul2006@gmail.com>
2023-10-02 19:40:49 +0000

    About complex lists

---
+++ mix-list/mixlist02.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+worldbank = ["indentation", "spaces"]
+tlgstudents = ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
+worldbank.append(4)
+choice = int(input("Pick astudent number !"))
+student_name = tlgstudents[choice]
+print(student_name + "always uses " + str(4) + " " + worldbank[1] + "to indent.") 
+
+
