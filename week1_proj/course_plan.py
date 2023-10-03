d53bbc6fc97e8c9f052e558d213a053a9e855ed5
akfathalla <abdul2006@gmail.com>
2023-10-03 22:09:24 +0000

    About complex lists

---
+++ week1_proj/course_plan.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+This program is intended for use by Computer Science students enrolling in classes beginning of Fall-2024 and intend to graduate on or earlier than 2027
+"""
+
+def main():
+
+
+
+
+
+
+
+
+    if __name__ == "__main__"
+    main()
