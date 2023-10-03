6002ef3bea13090cdc8ab326b9aae553bc89e526
akfathalla <abdul2006@gmail.com>
2023-10-03 20:02:37 +0000

    copying local files with python

--- montypython/monty_python3.py
+++ montypython/monty_python3.py
@@ -8,11 +8,11 @@ answer = " "
 while round < 3 and answer != "Brian":
     round += 1     # increase the round counter by 1
     answer = input('Finish the movie title, "Monty Python\'s The Life of ______": ')
-    answer = answer.title()
-    if answer == "Brian": # logic to check if user gave correct answer
-        print("Correct!")
-    elif answer == "shrubbery":
+
+    if answer == "shrubbery":
         print("You gave the super secret answer!")
+    elif answer == "Brian": # logic to check if user gave correct answer
+        print("Correct!")
     elif round == 3:    # logic to ensure round has not yet reached 3
         print("Sorry, the answer was Brian.")
     else:                 # if answer was wrong
